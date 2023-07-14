8b5061a00082e8ddc3a6fac682f3670135bc9052
Franck Bisschop <franck.bisschop@decathlon.com>
2023-07-14 17:27:21 +0200

    add dependencies react stack navigator

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
 import CategoriesScreen from './screens/CategoriesScreen';
 
 export default function App() {
   return (
     <>
       <StatusBar style='light' />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <CategoriesScreen />
+      </NavigationContainer>
     </>
-
   );
 }
 
